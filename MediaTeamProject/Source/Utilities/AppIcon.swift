47e722cdfb36ebe3e205142c0f276251916ba1bd
JunYeong-J <eeot2827@gmail.com>
2024-10-09 16:56:08 +0900

    Init: Utilities 초기설정 완료

---
+++ MediaTeamProject/Source/Utilities/AppIcon.swift
@@ -0,0 +1,21 @@
+//
+//  AppIcon.swift
+//  MediaTeamProject
+//
+//  Created by 전준영 on 10/9/24.
+//
+
+import UIKit
+
+enum AppIcon {
+    
+    static let play: UIImage? = UIImage(systemName: "play")
+    static let magnifyingGlass: UIImage? = UIImage(systemName: "magnifyingglass")
+    static let sparklesTV: UIImage? = UIImage(systemName: "sparkles.tv")
+    static let house: UIImage? = UIImage(systemName: "house")
+    static let playCircle: UIImage? = UIImage(systemName: "play.circle")
+    static let download: UIImage? = UIImage(systemName: "square.and.arrow.down")
+    static let faceSmiling: UIImage? = UIImage(systemName: "face.smiling")
+    static let plus: UIImage? = UIImage(systemName: "plus")
+    
+}
