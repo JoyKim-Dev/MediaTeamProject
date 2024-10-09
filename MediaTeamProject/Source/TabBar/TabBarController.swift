d5ed4f8c27521f24f75f4afbf43003b709f7f085
JunYeong-J <eeot2827@gmail.com>
2024-10-09 21:09:19 +0900

    Feat: 탭바 생성 및 뷰 컨트롤러 설정

---
+++ MediaTeamProject/Source/TabBar/TabBarController.swift
@@ -0,0 +1,64 @@
+//
+//  TabBarController.swift
+//  MediaTeamProject
+//
+//  Created by 전준영 on 10/9/24.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = AppColor.white
+        tabBar.unselectedItemTintColor = .gray
+        tabBar.backgroundColor = AppColor.blackRussian
+        
+        enum TabBase: Int, CaseIterable {
+            case home, topSearch, download
+            
+            var title: String {
+                switch self {
+                case .home:
+                    return AppStrings.TabBar.home
+                case .topSearch:
+                    return AppStrings.TabBar.topSearch
+                case .download:
+                    return AppStrings.TabBar.download
+                }
+            }
+            
+            var image: UIImage? {
+                switch self {
+                case .home:
+                    return AppIcon.house
+                case .topSearch:
+                    return AppIcon.magnifyingGlass
+                case .download:
+                    return AppIcon.faceSmiling
+                }
+            }
+            
+            var viewController: UIViewController {
+                switch self {
+                case .home:
+                    return SearchViewController()
+                case .topSearch:
+                    return SearchViewController()
+                case .download:
+                    return SearchViewController()
+                }
+            }
+        }
+        
+        let vc = TabBase.allCases.map { tabView in
+            let navItem = UINavigationController(rootViewController: tabView.viewController)
+            navItem.tabBarItem = UITabBarItem(title: tabView.title, image: tabView.image, tag: tabView.rawValue)
+            return navItem
+        }
+        
+        setViewControllers(vc, animated: true)
+    }
+}
---
+++ MediaTeamProject/Source/Utilities/AppStrings.swift
@@ -0,0 +1,18 @@
+//
+//  AppTabBarString.swift
+//  MediaTeamProject
+//
+//  Created by 전준영 on 10/9/24.
+//
+
+import Foundation
+
+enum AppTabBarString {
+    
+    enum TabBar {
+        static let home = "Home"
+        static let topSearch = "Top Search"
+        static let download = "Download"
+    }
+    
+}
