caa9c63eeb9f82e5e4d4a155def4a5bab42417f9
Joy <kimjoydev@gmail.com>
2024-10-09 13:02:39 +0900

    Initial Commit

---
+++ MediaTeamProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaTeamProject
+//
+//  Created by Joy Kim on 10/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
