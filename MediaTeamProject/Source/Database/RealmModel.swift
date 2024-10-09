900286fdc56e829a4bd1a30895102fc9754a888e
Joy <kimjoydev@gmail.com>
2024-10-09 17:51:54 +0900

    Init: Realm 초기설정

---
+++ MediaTeamProject/Source/Database/RealmModel.swift
@@ -0,0 +1,8 @@
+//
+//  RealmModel.swift
+//  MediaTeamProject
+//
+//  Created by Joy Kim on 10/9/24.
+//
+
+import Foundation
---
+++ MediaTeamProject/Source/Database/RealmRepository.swift
@@ -0,0 +1,8 @@
+//
+//  RealmRepository.swift
+//  MediaTeamProject
+//
+//  Created by Joy Kim on 10/9/24.
+//
+
+import Foundation
