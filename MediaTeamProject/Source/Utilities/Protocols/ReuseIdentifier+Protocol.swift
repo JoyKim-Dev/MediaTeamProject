8eb6fa5bfd0f1567a129c9a18749bd1e0ee7ec11
Joy <kimjoydev@gmail.com>
2024-10-10 13:54:42 +0900

    Style: Utilities 추가

--- MediaTeamProject/Source/Utilities/Protocols/ReuseIdentifier+Protocol.swift
+++ MediaTeamProject/Source/Utilities/Protocols/ReuseIdentifier+Protocol.swift
@@ -5,4 +5,16 @@
 //  Created by Joy Kim on 10/10/24.
 //
 
-import Foundation
+import UIKit
+
+protocol ReuseIdentifierProtocol: AnyObject {
+    
+    static var identifier: String {get}
+}
+
+extension UIView: ReuseIdentifierProtocol {
+
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
